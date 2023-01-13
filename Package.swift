6d5570fcd66904aa36fb78ac0619f15f7cbd9f7b
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2023-01-13 17:01:24 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Charts",
+            name: "UICharts",
             targets: ["Charts"]),
         .library(
             name: "ChartsDynamic",
